fc207399989a685cd4e2313dfd5f1dfba8a063fa
akshayb407 <akshayb407@gmail.com>
2022-07-10 16:10:41 +0530

    UC3 added

--- UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
+++ UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
@@ -6,16 +6,17 @@ namespace UserRegistrationInRegex
     {
         static void Main(string[] args)
         {
-         
+
             Console.WriteLine("Welcome to Regular Expression Program");
+
             ///First Name
             FirstName firstName = new FirstName();
-            Console.WriteLine("Enter First Nmae"); 
+            Console.WriteLine("Enter First Nmae");
             string ValidateFirstName = Console.ReadLine();
-              
+
             if (firstName.ValidateFirstName("Akshay"))
             {
-                
+
                 Console.WriteLine("The First Name is Valid");
             }
             else
@@ -31,6 +32,18 @@ namespace UserRegistrationInRegex
             }
             else
                 Console.WriteLine("The Last Name is Invalid");
+
+            ///Valid Email
+            Console.WriteLine("Enter Email");
+            string ValidEmail = Console.ReadLine();
+            ValidEmail checkEmail = new ValidEmail();
+            if (checkEmail.ValidateEMail("abc.xyz@bl.co.in"))
+            {
+                Console.WriteLine("The Email is Valid");
+            }
+            else
+                Console.WriteLine("The Email is Invalid");
+
 
         }
     }
---
+++ UserRegistrationInRegex/UserRegistrationInRegex/ValidEmail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationInRegex
+{
+    internal class ValidEmail
+    {
+        public static string REGEX_EMail = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$"; //abc.xyz@bl.co.in
+
+        public bool ValidateEMail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMail);
+        }
+    }
+}
