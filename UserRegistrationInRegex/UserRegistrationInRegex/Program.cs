8cea1ef95f3a999c8e73ce1d291163efe8773da7
akshayb407 <akshayb407@gmail.com>
2022-07-10 18:22:03 +0530

    UC6 added

--- UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
+++ UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
@@ -69,6 +69,20 @@ namespace UserRegistrationInRegex
             else
                 Console.WriteLine("The Password1 is Invalid");
 
+            ///Password 2 At Least One Upper Case
+            Console.WriteLine("Enter At Least One Upper Case ");
+            string Password2AtLeastOneUpperCase = Console.ReadLine();
+            var AtLeastOneUpperCase = new Password2AtLeastOneUpperCase();
+            if (AtLeastOneUpperCase.ValidatePassword2("AkshayBhagwat"))
+            {
+                Console.WriteLine("The Password2 is Valid");
+            }
+            else
+                Console.WriteLine("The Password2 is Invalid");
+
+
+
+
         }
     }
 }
