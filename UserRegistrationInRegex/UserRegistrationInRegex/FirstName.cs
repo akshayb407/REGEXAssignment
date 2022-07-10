513e78294179fe8ebbf60c10b5c00c8038af91c3
akshayb407 <akshayb407@gmail.com>
2022-07-10 15:48:14 +0530

    UC1 added

---
+++ UserRegistrationInRegex/UserRegistrationInRegex/FirstName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationInRegex
+{
+    internal class FirstName
+    {
+        public bool ValidateFirstName(string fName)
+        {
+            string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+            return Regex.IsMatch(fName, FIRST_NAME_REGEX);
+        }
+    }
+}
--- UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
+++ UserRegistrationInRegex/UserRegistrationInRegex/Program.cs
@@ -6,7 +6,20 @@ namespace UserRegistrationInRegex
     {
         static void Main(string[] args)
         {
+         
             Console.WriteLine("Welcome to Regular Expression Program");
+            ///First Name
+            FirstName firstName = new FirstName();
+            Console.WriteLine("Enter First Nmae"); 
+            string ValidateFirstName = Console.ReadLine();
+              
+            if (firstName.ValidateFirstName("Akshay"))
+            {
+                
+                Console.WriteLine("The First Name is Valid");
+            }
+            else
+                Console.WriteLine("The First Name is Invalid");
         }
     }
 }
